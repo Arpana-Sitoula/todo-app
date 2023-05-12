e4f5ee30c668f60b40d9c911a9e6822fd1408502
Arpana <arp2ctaula@gmail.com>
2023-05-12 14:17:35 +0545

    filter styling

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,15 +1,22 @@
+import { joinPaths } from '@remix-run/router';
 import React from 'react'
-import add from '../assets/add.png'
-import down from '../assets/down.gif'
 
-const AddTodo = ({value, name}) => {
-  return (
-    <div className='filter'>
-        <input type='text' value={value} name={name}/>
-        <div className='add'><img src={add}/></div>
-        <div className='down'><img src={down}/></div>
-    </div>
-  )
+const AddTodo = ({ value, name }) => {
+    return (
+        <div className='filter'>
+            <input type='text' value={value} name={name} />
+            <div className='add'>+</div>
+            <div class="dropdown">
+                <button class="dropbtn">Filter</button>
+                <div class="dropdown-content">
+                    <option>All</option>
+                    <option>Completed</option>
+                    <option>To do</option>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default AddTodo
--- src/components/resuable/Item.jsx
+++ src/components/resuable/Item.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
+const ITEM = [
+ {id:1, name:"Yoga", completed: false},
+ {id:2, name:"Drink Water", completed: true},
+ {id:3, name:"Journals", completed: true},
+ {id:4, name:"Learn React", completed: false},
+ {id:5, name:"Walk", completed: true}
+]
+
 const Item = () => {
   return (
-    <div>Item</div>
+    <div className='item'>
+ {ITEM.map((items) =>{
+          return(
+
+            // append class name by checking state i.e numbers and compairing it with number.id 
+            //use explicit prop names
+            <div  key={items.id}>{items.id} {items.name}</div>
+          )
+        })}
+    </div>
   )
 }
 
