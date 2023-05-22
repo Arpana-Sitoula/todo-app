22134583b8446a9ab22ee3582479d2482acd2667
Arpana <arp2ctaula@gmail.com>
2023-05-22 20:53:03 +0545

    Usereduce issue

--- src/page/TodoList.jsx
+++ src/page/TodoList.jsx
@@ -1,38 +1,68 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useReducer } from 'react'
 import AddTodo from '../components/AddTodo'
 import List from '../components/List'
-import Page from '../components/Page'
 
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'set_Input':
+      return { ...state, inputs: action.payload };
+    case 'set_Todos':
+      return { ...state, todos: action.payload };
+    case 'set_filter':
+      return { ...state, filter: action.payload };
+    case 'set_FilteredTodos':
+      return { ...state, filteredTodos: action.payload };
+    case 'set_Editing':
+      return { ...state, editing: action.payload };
+    default:
+      return;
+
+  }
+}
 
 
 const TodoList = () => {
-  const [inputs, setInput] = useState("");
-  const [todos, setTodos] = useState([]);
-  const [filter, setFilter] = useState("all");
-  const [filteredTodos, setFilteredTodos] = useState([]);
-  const [editing, setEditing] = useState(null);
+ const InitialState = [
+    {
+      inputs: "",
+      todos: [],
+      filter: "all",
+      filteredTodos: [],
+      editing: null
+    }
+  ]
+
+  const [state, dispatch] = useReducer(reducer, InitialState);
+
+  // const [inputs, setInput] = useState("");
+  // const [todos, setTodos] = useState([]);
+  // const [filter, setFilter] = useState("all");
+  // const [filteredTodos, setFilteredTodos] = useState([]);
+  // const [editing, setEditing] = useState(null);
+
 
   return (
     <div>
       <h1 className='center-div'>Todo List</h1>
       <AddTodo
-        inputs={inputs}
-        setInput={setInput}
-        setTodos={setTodos}
-        setFilter={setFilter}
-        editing={editing} 
-        />
+        inputs={state.inputs}
+        setInput={(inputs) => dispatch({ type: 'set_Input', payload: inputs })}
+        setTodos={(todos) => dispatch({ type: 'set_Todos', payload: todos })}
+        setFilter={(filter) => dispatch({ type: 'set_Filter', payload: filter })}
+        editing={state.editing}
+      />
 
       <List
-        inputs={inputs}
-        setInput={setInput}
-        todos={todos}
-        setTodos={setTodos}
-        filter={filter}
-        filteredTodos={filteredTodos}
-        setFilteredTodos={setFilteredTodos}
-        editing={editing}
-        setEditing={setEditing} />
+        inputs={state.inputs}
+        setInput={(inputs) => dispatch({ type: 'set_Inputs', payload: inputs })}
+        todos={state.todos}
+        setTodos={(todos) => dispatch({ type: 'set_Todos', payload: todos })}
+        filter={state.filter}
+        filteredTodos={state.filteredTodos}
+        setFilteredTodos={(filteredTodos) => dispatch({ type: 'set_Filter', payload: filteredTodos })}
+        editing={state.editing}
+        setEditing={(editing) => dispatch({ type: 'set_Editing', payload: editing })} />
 
     </div>
   )
